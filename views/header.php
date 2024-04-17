bc7d7370f000e84ad9914b462bb15923d695708c
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2024-04-17 19:54:52 +0700

    cm

--- views/header.php
+++ views/header.php
@@ -5,15 +5,16 @@
     <link rel="icon" href="../image/interview/favicon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../js/scripts.js">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../public/js/scripts.js">
+    <link rel="stylesheet" href="../public/css/bg.css">
 </head>
 <!--Main Navigation-->
 
 <body class="view intro-2">
     <header>
         <!-- <nav class="navbar navbar-expand-lg navbar-dark black fadeNavheader"> -->
-        <nav class="navbar navbar-expand-lg navbar-dark black">
+        <nav class="navbar navbar-expand-lg navbar-dark black fw-lighter">
             <div class="container ">
                 <a class="navbar-brand" href="#"><strong>MelodyTalent</strong></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- views/introduce.php
+++ views/introduce.php
@@ -1,16 +1,16 @@
 <main class="main-flex">
     <div>
         <div class="full-bg-img">
-            <div class="text-white fadeHomepage" style="text-shadow: 2px 3px 5px black;">
+            <div class="text-light fadeHomepage" style="text-shadow: 3px 4px 6px black;">
                 <div class="container">
                     <br><br><br><br><br><br><br><br>
                     <div class="text-center">
                         <h1><strong>Đôi lời giới thiệu về MelodyTalent</strong></h1>
                     </div><br><br>
-                    <div class="row text-center">
+                    <div class="row text-center fst-italic fw-lighter">
                         <div class="col-2"></div>
                         <div class="col-8">
-                            <a style="font-size: 24px;">Đây là trang Web rất hữu ích,giúp bạn học nhạc lý và nhạc cụ một cách nhanh chóng và hiệu quả. Với những giáo trình, công cụ mình hi vọng chúng sẽ hỗ trợ đắc lực và tạo cho bạn một nền nhạc lý cực kỳ vững chắc !!! </a>
+                            <a style="font-size: 34px;">Đây là trang Web rất hữu ích,giúp bạn học nhạc lý và nhạc cụ một cách nhanh chóng và hiệu quả. Với những giáo trình, công cụ mình hi vọng chúng sẽ hỗ trợ đắc lực và tạo cho bạn một nền nhạc lý cực kỳ vững chắc !!! </a>
                         </div>
                         <div class="col-2"></div>
                     </div><br><br><br>
