decab62601b76dc00d5e027765a8cb02a2762811
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2024-04-30 00:02:26 +0700

    a

--- view/header.php
+++ view/header.php
@@ -8,19 +8,18 @@
     <link rel="stylesheet" href="../public/css/style.css">
     <link rel="stylesheet" href="../public/js/scripts.js">
     <link rel="stylesheet" href="../public/css/bg.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <!--Main Navigation-->
 
 <body class="view intro-2">
     <header>
-        <!-- <nav class="navbar navbar-expand-lg navbar-dark black fadeNavheader  fw-lighter"> -->
         <nav class="navbar navbar-expand-lg navbar-dark black fst-italic">
             <div class="container ">
                 <a class="navbar-brand" href="#"><strong>MelodyTalent</strong></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <!-- <div class="collapse navbar-collapse fadeNavheader" id="navbarSupportedContent" style="text-shadow: 1px 2px 4px black;"> -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent" style="text-shadow: 1px 2px 4px black;font-size:18px;">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active ">
--- view/login.php
+++ view/login.php
@@ -1,130 +1,75 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Login Form</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .custom-container {
-            max-width: 500px;
-            margin: 0 auto;
-            margin-top: 100px;
-            background-color: #ffffff;
-            padding: 30px;
-            border-radius: 4px;
-        }
-
-        .custom-tab-content {
-            margin-top: 20px;
-        }
-
-        .custom-tab-pane {
-            padding: 30px;
-            background-color: #f9f9f9;
-            border-radius: 4px;
-        }
-
-        .custom-form-group label {
-            font-weight: bold;
-        }
-
-        .custom-btn-primary {
-            width: 100%;
-        }
-
-        .custom-nav-tabs {
-            margin-bottom: 20px;
-        }
-
-        .custom-nav-link {
-            color: #000000;
-            padding: 10px 20px;
-            font-weight: bold;
-            font-size: 18px;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="custom-container fst-italic fadeHomepage" style="margin-top:-650px">
-        <div class="card">
-            <div class="card-header">
-                <ul class="nav custom-nav-tabs justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link active custom-nav-link" id="custom-login-tab" data-toggle="tab" href="#custom-login">Đăng nhập</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link custom-nav-link" id="custom-register-tab" data-toggle="tab" href="#custom-register">Đăng ký</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="card-body">
-                <div class="tab-content custom-tab-content">
-                    <div class="tab-pane fade show active" id="custom-login">
-                        <h3 style="color: black;">Đăng nhập</h3>
-                        <form id="loginForm">
-                            <div class="form-group custom-form-group">
-                                <label for="loginName">Email:</label>
-                                <input type="email" class="form-control" id="loginName">
-                            </div>
-                            <div class="form-group custom-form-group">
-                                <label for="loginPassword">Mật khẩu:</label>
-                                <input type="password" class="form-control" id="loginPassword">
-                            </div>
-                            <button type="submit" class="btn btn-primary custom-btn-primary">Đăng nhập</button>
-                        </form>
-                    </div>
-                    <div class="tab-pane fade" id="custom-register">
-                        <h3 style="color: black;">Đăng ký</h3>
-                        <form id="registerForm">
-                            <div class="form-group custom-form-group">
-                                <label for="registerEmail">Email:</label>
-                                <input type="email" class="form-control" id="registerEmail">
-                            </div>
-                            <div class="form-group custom-form-group">
-                                <label for="registerPassword">Mật khẩu:</label>
-                                <input type="password" class="form-control" id="registerPassword">
-                            </div>
-                            <div class="form-group custom-form-group">
-                                <label for="confirmPassword">Xác nhận mật khẩu:</label>
-                                <input type="password" class="form-control" id="confirmPassword">
-                            </div>
-                            <button type="submit" class="btn btn-primary custom-btn-primary">Đăng ký</button>
-                        </form>
-                    </div>
+<link rel="stylesheet" href="../public/css/login.css">
+<br>
+<div class="custom-container rounded">
+    <ul class="nav nav-tabs justify-content-center ">
+        <li class="nav-item">
+            <a class="nav-link text-dark active" id="custom-login-tab" data-bs-toggle="tab" href="#custom-login">Đăng nhập</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" id="custom-register-tab" data-bs-toggle="tab" href="#custom-register">Đăng ký</a>
+        </li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-pane fade show active" id="custom-login">
+            <h3 class="mt-3">Đăng nhập</h3>
+            <form id="loginForm" class="mt-4">
+                <div class="mb-3">
+                    <label for="loginName" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="loginName">
                 </div>
-            </div>
+                <div class="mb-3">
+                    <label for="loginPassword" class="form-label">Mật khẩu:</label>
+                    <input type="password" class="form-control" id="loginPassword">
+                </div>
+                <button type="submit" class="btn btn-primary text-black">Đăng nhập</button>
+            </form>
+        </div>
+        <div class="tab-pane fade" id="custom-register">
+            <h3 class="mt-3">Đăng ký</h3>
+            <form id="registerForm" class="mt-4">
+                <div class="mb-3">
+                    <label for="registerEmail" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="registerEmail">
+                </div>
+                <div class="mb-3">
+                    <label for="registerPassword" class="form-label">Mật khẩu:</label>
+                    <input type="password" class="form-control" id="registerPassword">
+                </div>
+                <div class="mb-3">
+                    <label for="confirmPassword" class="form-label">Xác nhận mật khẩu:</label>
+                    <input type="password" class="form-control" id="confirmPassword">
+                </div>
+                <button type="submit" class="btn btn-primary">Đăng ký</button>
+            </form>
         </div>
     </div>
-<br><br><br><br><br><br><br><br>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            // Lấy form đăng nhập
-            var loginForm = document.getElementById('loginForm');
+</div>
 
-            // Xử lý sự kiện submit form
-            loginForm.addEventListener('submit', function(event) {
-                event.preventDefault(); // Ngăn chặn form submit
 
-                // Lấy giá trị email và password
-                var email = document.getElementById('loginName').value;
-                var password = document.getElementById('loginPassword').value;
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Lấy form đăng nhập
+        var loginForm = document.getElementById('loginForm');
 
-                // In giá trị email và password ra console
-                console.log('Email:', email);
-                console.log('Password:', password);
+        // Xử lý sự kiện submit form
+        loginForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Ngăn chặn form submit
 
-                // Thực hiện các tác vụ khác sau khi lấy giá trị email và password
-                // ...
+            // Lấy giá trị email và password
+            var email = document.getElementById('loginName').value;
+            var password = document.getElementById('loginPassword').value;
 
-                // Reset form
-                loginForm.reset();
-            });
+            // In giá trị email và password ra console
+            console.log('Email:', email);
+            console.log('Password:', password);
+
+            // Thực hiện các tác vụ khác sau khi lấy giá trị email và password
+            // ...
+
+            // Reset form
+            loginForm.reset();
         });
-    </script>
-</body>
+    });
+</script>
 
-</html
+<br><br><br><br><br><br><br><br><br><br><br><br>
