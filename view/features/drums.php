f1b0ba9ac7d6537ad7bdc118d105427cbbfcd208
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2024-09-17 18:05:01 +0700

    drumsssss

--- view/features/drums.php
+++ view/features/drums.php
@@ -1,56 +1,63 @@
-<head>
-    <link href="media/vendor/joomla-custom-elements/css/joomla-alert.min44b7.css?0.2.0" rel="stylesheet" />
-    <link href="modules/mod_ads/css/min/style.min5034.css?v=202402141905" rel="stylesheet" />
-    <script src="media/vendor/jquery/js/jquery.min84e3.js?3.6.0"></script>
-    <script src="modules/mod_ads/js/min/width_test.min5034.js?v=202402141905"></script>
-    <link rel="stylesheet" href="templates/musicca/css/screen.mine4c3.css?v=?v=20240402160335">
-    <script defer src="templates/musicca/js/app.minc683.js?v=?v=20231207215500"></script>
-
-</head>
-
-<div class="fix__content">
-    <div class="wrapper bg-white">
+<link href="../library/drums/style.min5034.css?v=202402141905" rel="stylesheet" />
+<link href="../library/drums/1.css" rel="stylesheet" />
+<script src="../library/drums/jquery.min84e3.js?3.6.0"></script>
+<script defer src="../library/drums/app.minc683.js?v=?v=20231207215500"></script>
+<div class="drumss bg-white" >
+    <div class="wrapper">
         <div class="content">
-            <div class="container">
+            <div class="container" >
                 <div class="row flex__row">
-                    <div class="content__right col-9-m col-">
+                    <aside class="navigation__left col-3-m">
+                        <div class="ads-box_navigation" data-ads-box="navigation">
+                            <div class="ads-body">
+                                <div data-fuse="22742093407"></div>
+                            </div>
+                        </div>
+                    </aside>
+                    <div class="ads-box_t0p" data-ads-box="top">
+                        <div>
+                            <div class="ads-body">
+                                <div data-fuse="22658345925"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="content__right col-9-m col-" style="margin-top:-400px;" >                         -->
+                    <div class="content__right col-9-m col-"  >
                         <div id="system-message-container" aria-live="polite"></div>
                         <article class="default__content item-page " itemscope itemtype="https://schema.org/Article">
-                            <meta itemprop="inLanguage" content="en-GB">
+                            <meta itemprop="inLanguage" content="vi-VN">
+
                             <div class="page-header default__page__header">
-                                <h1 class="h1 default__page__title" itemprop="name">
-                                    Virtual drums </h1>
+                                <h1 class="h1 default__page__title text-center" itemprop="name">
+                                    Trống ảo </h1>
                             </div>
                             <div itemprop="articleBody">
-                                <div class="default__page__content">
-                                    <link href="files/scripts/drums/static/css/main.css" rel="stylesheet" />
-                                    <noscript>
-                                        You need to enable JavaScript to use the virtual drums.
-                                    </noscript>
+                                <div class="default__page__content" >
+                                    <link href="../library/drums/drums.css" rel="stylesheet" />
                                     <div id="drums"></div>
-                                    <script src="files/scripts/drums/translation/English.js"></script>
-                                    <script src="files/scripts/drums/static/js/main.js"></script>
-                                    <script src="files/scripts/drums/static/js/main.chunk.js"></script>
+                                    <script src="../library/drums/Vietnamese.js"></script>
+                                    <script src="../library/drums/drums.js"></script>
+                                    <script src="../library/drums/main.chunk.js"></script>
                                     <div class="tools__page__content">
-                                        <h2>Play drums online</h2>
-                                        <p>Play the virtual drums using your keyboard or by clicking on the drums. Click the "Drum kit" button above the drums to change the drum kit. Click "Show shortcuts" to see the shortcuts for the drums and cymbals. You can use the following shortcuts:</p>
+                                        <h2>Chơi trống trực tuyến</h2>
+                                        <p>Sử dụng bàn phím máy vi tính của bạn hoặc nhấp vào dàn trống để chơi trống ảo. Nhấp vào nút "Thể hiện các phím tắt" bên trên dàn trống để xác định các phím tắt dành cho các loại trống và chũm chọe trong dàn trống. Bạn có thể sử dụng các phím tắt sau đây:</p>
                                         <ul class="liste" style="margin-top:0px">
-                                            <li>Crash cymbal: <span class="shortcut-code">y</span></li>
-                                            <li>Ride cymbal: <span class="shortcut-code">u</span></li>
-                                            <li>Hi-hat (closed): <span class="shortcut-code">r</span> or <span class="shortcut-code">t</span></li>
-                                            <li>Hi-hat (open): <span class="shortcut-code">e</span> or <span class="shortcut-code">w</span></li>
-                                            <li>Hi-hat (foot): <span class="shortcut-code">c</span></li>
-                                            <li>High tom-tom: <span class="shortcut-code">g</span></li>
-                                            <li>Low tom-tom: <span class="shortcut-code">h</span></li>
-                                            <li>Floor tom: <span class="shortcut-code">j</span></li>
-                                            <li>Snare drum: <span class="shortcut-code">s</span> or <span class="shortcut-code">a</span></li>
-                                            <li>Snare drum (cross stick): <span class="shortcut-code">d</span> or <span class="shortcut-code">f</span></li>
-                                            <li>Bass drum: <span class="shortcut-code">x</span> or <span class="shortcut-code">z</span></li>
+                                            <li>Chũm chọe trung: <span class="shortcut-code">y</span></li>
+                                            <li>Chũm chọe to: <span class="shortcut-code">u</span></li>
+                                            <li>Chũm chọe hi-hat (đóng): <span class="shortcut-code">r</span> hay <span class="shortcut-code">t</span></li>
+                                            <li>Chũm chọe hi-hat (mở): <span class="shortcut-code">e</span> hay <span class="shortcut-code">w</span></li>
+                                            <li>Chũm chọe hi-hat (chân): <span class="shortcut-code">c</span></li>
+                                            <li>Trống tom-tom bên trên: <span class="shortcut-code">g</span></li>
+                                            <li>Trống tom-tom bên dưới: <span class="shortcut-code">h</span></li>
+                                            <li>Trống floor tom: <span class="shortcut-code">j</span></li>
+                                            <li>Trống lẫy: <span class="shortcut-code">s</span> hay <span class="shortcut-code">a</span></li>
+                                            <li>Trống lẫy (kỹ thuật dùi chéo): <span class="shortcut-code">d</span> hay <span class="shortcut-code">f</span></li>
+                                            <li>Trống bass: <span class="shortcut-code">x</span> hay <span class="shortcut-code">z</span></li>
                                         </ul>
-                                        <p>Drummers often use their right hand to play the cymbals (crash cymbal, ride cymbal, and hi-hat), and their left hand to play the snare drum. When using shortcuts, use the left hand to play both the snare drum and the bass drum.</p>
-                                        <p>Play the virtual drums, and then apply your new skills to real drums. Use the <a href="drum-machine.html">drum machine</a> to find inspiration for drum beats and rhythm patterns. A fun way to practice with the virtual drums is by playing along with your favorite songs.</p>
-                                        <h2>Free online rhythm exercises</h2>
-                                        <p>Try our <a href="exercises/rhythms.html">free rhythm exercises</a> and learn to read, write, and play rhythms. You'll also find a variety of other exercises that will expand your musical understanding and help you become better at playing the drums.</p>
+                                        <p>Các tay chơi trống thường dùng bàn tay phải để chơi các chũm chọe (chũm chọe trung, chũm chọe to and chũm chọe hi-hat) và dùng tay trái để chơi trống lẫy. Khi sử dụng các phím tắt, hãy dùng tay trái để chơi cả trống lẫy và trống bass.</p>
+                                        <p>Hãy chơi trống ảo và áp dụng các kỹ năng mà bạn mới học được để chơi trống thật. Hãy sử dụng <a href="https://www.musicca.com/drum-machine">máy đánh trống</a> để tìm cảm hứng ở các nhịp trống và các khuôn mẫu nhịp điệu. Một cách tạo cảm hứng khi tập chơi trống ảo là chơi trống ảo dựa trên những bài hát ưa thích của bạn.</p>
+                                        <h2>Các bài tập luyện nhịp điệu trực tuyến miễn phí</h2>
+                                        <p>Hãy thử tiến hành <a href="bai-tap/cac-loai-nhip-dieu.html">các bài tập luyện nhịp điệu miễn phí của chúng tôi</a> và học cách đọc, ghi lại và chơi các loại nhịp điệu. Bạn cũng sẽ tìm thấy rất nhiều bài tập nhạc khác có khả năng phát triển sự hiểu biết âm nhạc của bạn và giúp bạn chơi các loại trống thiện nghệ hơn.</p>
                                     </div>
                                 </div>
                             </div>
@@ -63,5 +70,35 @@
             </div>
         </div>
     </div>
-
 </div>
+<!-- <style>
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown button {
+        background: #f9f9f9;
+        border: 1px solid #ccc;
+        padding: 5px 10px;
+        cursor: pointer;
+    }
+
+    .dropdown ul {
+        display: none;
+        position: absolute;
+        background: #fff;
+        padding: 0;
+        margin: 0;
+    }
+
+    .dropdown ul.no-bullet {
+        list-style-type: none;
+        padding-left: 0;
+    }
+
+    .dropdown ul li {
+        display: block;
+        margin-left:- 15em;
+    }
+</style> -->
