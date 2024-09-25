8fdb10d017306ca600bd2d5fe716cdac7f938572
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2024-09-26 00:19:20 +0700

    ok

--- view/features/guitar.php
+++ view/features/guitar.php
@@ -19,7 +19,7 @@
 								<div id="classic-guitar"></div>
 								<script src="../library/guitar/files/scripts/virtual-guitar/translation/Vietnamese.js"></script>
 								<script src="../library/guitar/files/scripts/virtual-guitar/static/js/chunk-vendors.js"></script>
-								<script src="../library/guitar/files/scripts/virtual-guitar/static/js/t.js"></script>
+								<script src="../library/guitar/files/scripts/virtual-guitar/static/js/ttt.js"></script>
 							</div>
 						</div>
 					</article>
